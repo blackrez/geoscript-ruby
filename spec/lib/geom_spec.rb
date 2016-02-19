4d050cc9beaa778547c03847f6ac37ce7c0b6c26
Nabil Servais <nabil@eml.cc>
2016-02-19 23:18:47 +0100

    Add more tests on Point

--- spec/lib/geom_spec.rb
+++ spec/lib/geom_spec.rb
@@ -4,9 +4,23 @@ include GeoScript::Geom
 
 describe Point do
   describe "#wkt" do
-    it "return a wkt from a Point" do
+    it "return a wkt from a Point instance" do
       p = Point.new -111.0, 45.7
       expect(p.to_wkt).to eq("POINT (-111 45.7)")
+    end
+  end
+
+  describe "#wkb" do
+    it "return a wkb from a Point instance" do
+      p = Point.new -111.0, 45.7
+      expect(p.to_wkb).to eq("0000000001C05BC000000000004046D9999999999A")
+    end
+  end
+
+  describe "#json" do
+    it "return a wkb from a Point instance" do
+      p = Point.new -111.0, 45.7
+      expect(p.to_json).to eq("{\"type\":\"Point\",\"coordinates\":[-111,45.7]}")
     end
   end
 end
