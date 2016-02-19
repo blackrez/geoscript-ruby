ceeb256000387582f08cd472964471f62aa7c80b
Nabil Servais <nabil@eml.cc>
2016-02-19 23:07:33 +0100

    Remove deprecation warning

--- spec/lib/geom_spec.rb
+++ spec/lib/geom_spec.rb
@@ -6,7 +6,7 @@ describe Point do
   describe "#wkt" do
     it "return a wkt from a Point" do
       p = Point.new -111.0, 45.7
-      p.to_wkt.should == "POINT (-111 45.7)"
+      expect(p.to_wkt).to eq("POINT (-111 45.7)")
     end
   end
 end
--- spec/lib/geoscript_spec.rb
+++ spec/lib/geoscript_spec.rb
@@ -4,8 +4,8 @@ require_relative '../../lib/geoscript'
 describe GeoScript do
   describe '#version' do
     it "return the right version" do
-      GeoScript.version.should ==  "0.1.0.pre"
-      GeoScript.geotools_version.should == "10.0"
+      expect(GeoScript.version).to eq("0.1.0.pre")
+      expect(GeoScript.geotools_version).to eq("10.0")
     end
   end
 end
