4fc38b68df64a8ec2a406da63c0b96e425d7e681
Nabil Servais <nabil@eml.cc>
2016-03-20 21:58:01 +0100

    Upgrade to the last geotools

--- spec/lib/geoscript_spec.rb
+++ spec/lib/geoscript_spec.rb
@@ -5,7 +5,7 @@ describe GeoScript do
   describe '#version' do
     it "return the right version" do
       expect(GeoScript.version).to eq("0.1.0.pre")
-      expect(GeoScript.geotools_version).to eq("10.0")
+      expect(GeoScript.geotools_version).to eq("15-SNAPSHOT")
     end
   end
 end
